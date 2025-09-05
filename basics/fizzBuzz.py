88be816f45b652b81ec57cfbd60586be5f2a32a1
Ben Grimmius <bgrimmdev@gmail.com>
2025-09-05 15:27:34 -0700

    Concise fizzbuzz

--- basics/fizzBuzz.py
+++ basics/fizzBuzz.py
@@ -7,12 +7,11 @@ FizzBuzz Rules:
 - If the number is divisible by both 3 and 5 → print "FizzBuzz"
 """
 
+
 for i in range(1, 101):
-    if (i % 3 == 0 and i % 5 == 0):
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    print(output or i)
