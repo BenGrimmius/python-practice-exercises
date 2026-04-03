1cfa03ce8266aa341f4558f47bb57cbf216e68bb
Ben Grimmius <bgrimmdev@gmail.com>
2026-04-03 14:42:52 -0700

    complete file handling challenge

--- basics/file_handling_exercise/analyze_logs.py
+++ basics/file_handling_exercise/analyze_logs.py
@@ -1,9 +1,12 @@
-with open("server.log") as logFile:
-    errorLineCount: int = 0
-    for line in logFile:
-        if "ERROR" in line:
-            errorLineCount += 1
-            with open("errors.txt", "w") as file:
-                file.write(line)
-    print(f"There are {errorLineCount} errors")
+with open("server.log") as log_file:
+    errorCount: int = 0
+    warningCount: int = 0
 
+    with open("errors.txt", "w") as error_file:
+        for line in log_file:
+            if "WARNING" in line:
+                warningCount += 1
+            if "ERROR" in line:
+                errorCount += 1
+                error_file.write(line)
+print(f"Total Errors: {errorCount}\nTotal Warnings: {warningCount}")
