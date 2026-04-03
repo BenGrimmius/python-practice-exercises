e092a7b502517cb2b242cf990b5b6adf0729dfbf
Ben Grimmius <bgrimmdev@gmail.com>
2026-04-03 16:43:37 -0700

    Started counting error lines and printing them

--- basics/file_handling_exercise/analyze_logs.py
+++ basics/file_handling_exercise/analyze_logs.py
@@ -1,12 +1,21 @@
-with open("server.log") as log_file:
-    errorCount: int = 0
-    warningCount: int = 0
+try:
+    with open("server.log") as log_file:
+        error_count: int = 0
+        warning_Count: int = 0
 
-    with open("errors.txt", "w") as error_file:
-        for line in log_file:
-            if "WARNING" in line:
-                warningCount += 1
-            if "ERROR" in line:
-                errorCount += 1
-                error_file.write(line)
-print(f"Total Errors: {errorCount}\nTotal Warnings: {warningCount}")
+        with open("errors.txt", "w+") as error_file:
+
+            for line in log_file:
+                if "WARNING" in line:
+                    warning_Count += 1
+                elif  "ERROR" in line:
+                    error_count += 1
+                    error_file.write(line)
+            first_line: str = error_file.readline()
+            print(first_line)
+
+            print(f"First error line: {first_line}")
+
+    print(f"Total Errors: {error_count}\nTotal Warnings: {warning_Count}")
+except FileNotFoundError:
+    print("Log file not found.")
