a3eec0044c6ce95815253278a30f9435365a75ea
Ben Grimmius <bgrimmdev@gmail.com>
2026-04-04 15:24:24 -0700

    Completed finding last and first errors

--- basics/file_handling_exercise/analyze_logs.py
+++ basics/file_handling_exercise/analyze_logs.py
@@ -1,6 +1,7 @@
 try:
     with open("server.log") as log_file:
         error_count: int = 0
+        error_line_points = []
         warning_count: int = 0
 
         with open("errors.txt", "w+") as error_file:
@@ -9,12 +10,17 @@ try:
                 if "WARNING" in line:
                     warning_count += 1
                 elif  "ERROR" in line:
+                    error_line_points.append(error_file.tell())
                     error_count += 1
                     error_file.write(line)
+
             error_file.seek(0)
             first_line: str = error_file.readline()
 
-            print(f"First error line: {first_line}\nLast error line: {error_file.readlines()[-1]}")
+            error_file.seek(error_line_points[-1])
+            last_line = error_file.readline()
+
+            print(f"First error line: {first_line}\nLast error line: {last_line}")
 
     print(f"Total Errors: {error_count}\nTotal Warnings: {warning_count}")
 except FileNotFoundError:
