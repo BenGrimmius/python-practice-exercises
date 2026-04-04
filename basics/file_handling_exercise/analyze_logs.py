f4aa7e69293e3dc42a226d2f000d421e56e42a22
Ben Grimmius <bgrimmdev@gmail.com>
2026-04-04 13:04:22 -0700

    Need to change the way i am selecting the last ERROR

--- basics/file_handling_exercise/analyze_logs.py
+++ basics/file_handling_exercise/analyze_logs.py
@@ -1,21 +1,21 @@
 try:
     with open("server.log") as log_file:
         error_count: int = 0
-        warning_Count: int = 0
+        warning_count: int = 0
 
         with open("errors.txt", "w+") as error_file:
 
             for line in log_file:
                 if "WARNING" in line:
-                    warning_Count += 1
+                    warning_count += 1
                 elif  "ERROR" in line:
                     error_count += 1
                     error_file.write(line)
+            error_file.seek(0)
             first_line: str = error_file.readline()
-            print(first_line)
 
-            print(f"First error line: {first_line}")
+            print(f"First error line: {first_line}\nLast error line: {error_file.readlines()[-1]}")
 
-    print(f"Total Errors: {error_count}\nTotal Warnings: {warning_Count}")
+    print(f"Total Errors: {error_count}\nTotal Warnings: {warning_count}")
 except FileNotFoundError:
     print("Log file not found.")
